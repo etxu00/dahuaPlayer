e1f1299690ad042070d1679d1b7722e01aa6936d
etxu00 <etxu.00@gmail.com>
2025-09-03 15:52:46 -0600

    feat: agregar botón para reiniciar y limpiar tokens en ImouPlayer

--- src/components/ImouPlayer.vue
+++ src/components/ImouPlayer.vue
@@ -200,6 +200,14 @@ function iniciarStream() {
   window.player = player
 }
 
+function reset() {
+  // Elimina toda la información de localStorage y recarga la página
+  localStorage.removeItem('tokens_imou');
+  localStorage.removeItem('token_bear');
+  localStorage.removeItem('token_bear_expiration');
+  window.location.reload();
+}
+
 async function inicio() {
   try {
     await obtenerParametrosURL()
@@ -502,6 +510,17 @@ onMounted(() => {
             <path d="M14 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
           </svg>
           <span>Iniciar cuadrícula</span>
+        </button>
+        <button @click="reset">
+          <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M3.06 13a9 9 0 1 0 .49 -4.087" />
+            <path d="M3 4.001v5h5" />
+            <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+          </svg>
+          <span>Forzar reintento</span>
         </button>
       </div>
     </div>
