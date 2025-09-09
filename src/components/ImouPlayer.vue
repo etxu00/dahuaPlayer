fa9b0af15a98ab0dd36f9cdfa68bb0c729e3c3ab
etxu00 <etxu.00@gmail.com>
2025-09-09 12:59:26 -0600

    feat: ajustar posición del canvas en el componente ImouPlayer

--- src/components/ImouPlayer.vue
+++ src/components/ImouPlayer.vue
@@ -500,7 +500,7 @@ onMounted(() => {
           <span>Detener grabación de pantalla</span>
         </button>
         <button @click="initGrid">
-          <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
             fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -537,12 +537,14 @@ onMounted(() => {
 *, *:before, *:after {
   box-sizing: border-box;
 }
-
 body {
   margin: 0;
   font-family: Arial, sans-serif;
   background-color: black;
   color: white;
+}
+.kind-stream-canvas.video-player-canvas {
+  left: 0;
 }
 </style>
 <style scoped>
